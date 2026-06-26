e9c9cc84c48e5b7a62750a0e5867a4910883d36a
Mel Stephen Perez <mel.perez.5203@gmail.com>
2026-06-26 20:27:34 +0800

    chore: implemented user login, registration, and DB conneciton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'config/supabase_config.dart';
 import 'views/auth/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
+
   runApp(const DevEatsApp());
 }
 
@@ -11,8 +24,8 @@ class DevEatsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DevEats',
       debugShowCheckedModeBanner: false,
+      title: "DevEats",
       theme: ThemeData(
         fontFamily: 'Roboto',
       ),
